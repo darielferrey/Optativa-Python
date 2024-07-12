795e00e836ae271289a742004d98ce148b86795d
Dariel Fdez Rey <darielferrey@gmail.com>
2024-07-12 10:04:08 -0400

    Add files via upload

---
+++ bfs.py
@@ -0,0 +1,28 @@
+from collections import defaultdict, deque
+
+def bfs(graph, start_node):
+    visited = set() 
+    queue = deque([start_node])  
+
+    while queue:
+        current_node = queue.popleft()  
+        if current_node not in visited:
+            print(current_node)  
+            visited.add(current_node)  
+
+           
+            for neighbor in graph.get(current_node, []):
+                if neighbor not in visited:
+                    queue.append(neighbor)
+
+graph = {
+    'A': ['B', 'C'],
+    'B': ['D', 'E'],
+    'C': ['F'],
+    'D': [],
+    'E': ['F'],
+    'F': []
+}
+
+start_node = 'A'
+bfs(graph, start_node)
